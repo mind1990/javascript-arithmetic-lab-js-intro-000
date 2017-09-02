d23e328fb1799251534c3ef55a11f1b154665826
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-02 16:17:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ function subtract(a, b) {
 function multiply(a, b) {
   return a * b
 }
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(a, b) {
+  return
+}
+
+function dec(a, b) {
+  return
+}
